8d772934cdf258ec806e4f6445659f92f6fc5cfa
Miroslav Kravec <kravec.miroslav@gmail.com>
2018-07-01 09:19:45 +0200

    Release version 0.2.0

--- bin/generateHelpDocs.sh
+++ bin/generateHelpDocs.sh
@@ -7,15 +7,15 @@ cd "${PROJECT_DIRECTORY}"
 
 echo -e "# \`svg-labels\` help\n\n\`\`\`" > docs/help.md
 # TODO: use correct version
-java -jar  ./tool/build/libs/tool-0.2.0-SNAPSHOT-executable.jar --help >> docs/help.md
+java -jar  ./tool/build/libs/tool-0.2.0-executable.jar --help >> docs/help.md
 echo -e "\`\`\`" >> docs/help.md
 
 echo -e "\nCommand \`tile\`:\n\`\`\`" >> docs/help.md
 # TODO: use correct version
-java -jar  ./tool/build/libs/tool-0.2.0-SNAPSHOT-executable.jar tile --help >> docs/help.md
+java -jar  ./tool/build/libs/tool-0.2.0-executable.jar tile --help >> docs/help.md
 echo -e "\`\`\`" >> docs/help.md
 
 echo -e "\nCommand \`instance\`:\n\`\`\`" >> docs/help.md
 # TODO: use correct version
-java -jar  ./tool/build/libs/tool-0.2.0-SNAPSHOT-executable.jar instance --help >> docs/help.md
+java -jar  ./tool/build/libs/tool-0.2.0-executable.jar instance --help >> docs/help.md
 echo -e "\`\`\`" >> docs/help.md
