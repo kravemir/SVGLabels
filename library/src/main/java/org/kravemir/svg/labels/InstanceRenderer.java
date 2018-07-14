ae155a7f5e9343ffcfe591b4856b3b484e4e3783
Miroslav Kravec <kravec.miroslav@gmail.com>
2018-07-14 19:08:48 +0200

    Documentation: describe API InstanceRenderer#render

--- library/src/main/java/org/kravemir/svg/labels/InstanceRenderer.java
+++ library/src/main/java/org/kravemir/svg/labels/InstanceRenderer.java
@@ -8,6 +8,14 @@ import java.util.Map;
 @ToBePublicApi
 public interface InstanceRenderer {
 
+    /**
+     * Renders instance as SVG from provided SVG template, template descriptor, and instance content
+     *
+     * @param svgTemplate template to use for rendering
+     * @param templateDescriptor descriptor for {@code svgTemplate} input argument
+     * @param instanceContent map containing instance values for attributes in template
+     * @return rendered SVG
+     */
     @ToBePublicApi
     String render(String svgTemplate,
                   LabelTemplateDescriptor templateDescriptor,
