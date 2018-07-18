40a351fd59ceec1b9cb640577a2b7bccfe91f7b0
Miroslav Kravec <kravec.miroslav@gmail.com>
2018-07-18 19:34:30 +0200

    Move all test resources into resources of testlib

--- tool/src/test/java/org/kravemir/svg/labels/tool/ToolRunnerTest.java
+++ tool/src/test/java/org/kravemir/svg/labels/tool/ToolRunnerTest.java
@@ -1,6 +1,7 @@
 package org.kravemir.svg.labels.tool;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,9 +10,9 @@ import org.kravemir.svg.labels.utils.RenderingUtils;
 import org.w3c.dom.Document;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Paths;
+import java.io.OutputStream;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
@@ -30,36 +31,60 @@ public class ToolRunnerTest {
         outputFile = folder.newFile("testOutput");
     }
 
+    // TODO: refactor to proper place
+    // TODO: think about descritor path...
+    private File getTemplateFromResource(String name, boolean withDescriptorJSON) throws IOException {
+        File copyFile = folder.newFile();
+        OutputStream copyFileStream = new FileOutputStream(copyFile);
+        IOUtils.copy(
+                getClass().getResource(name).openStream(),
+                copyFileStream
+        );
+        copyFileStream.close();
+
+        if(withDescriptorJSON) {
+            File copyFileDescriptor = folder.newFile(copyFile.getName() + "-labels.json");
+            OutputStream copyFileDescriptorStream = new FileOutputStream(copyFileDescriptor);
+            IOUtils.copy(
+                    getClass().getResource(name + "-labels.json").openStream(),
+                    copyFileDescriptorStream
+            );
+            copyFileDescriptorStream.close();
+        }
+
+        return copyFile;
+    }
+
     @Test
-    public void testRenderWithoutInstance() throws URISyntaxException, IOException {
+    public void testRenderWithoutInstance() throws IOException {
         ToolRunner.main( new String[]{
                 "--paper-size", "210", "297",
                 "--label-offset", "0", "0",
                 "--label-size", "65", "26.5",
                 "--label-delta", "0", "0",
-                Paths.get(getClass().getResource("/template01.svg").toURI()).toFile().getAbsolutePath(),
+                getTemplateFromResource("/template01.svg", true).getAbsolutePath(),
                 outputFile.getAbsolutePath()
         });
         System.out.println(FileUtils.readFileToString(outputFile));
     }
 
     @Test
-    public void testRenderWithInstance() throws URISyntaxException, IOException {
+    public void testRenderWithInstance() throws IOException {
         ToolRunner.main( new String[]{
                 "--paper-size", "210", "297",
                 "--label-offset", "0", "0",
                 "--label-size", "65", "26.5",
                 "--label-delta", "0", "0",
                 "--instance-json",
-                Paths.get(getClass().getResource("/test-instance.json").toURI()).toFile().getAbsolutePath(),
-                Paths.get(getClass().getResource("/template01.svg").toURI()).toFile().getAbsolutePath(),
+                getTemplateFromResource("/test-instance.json", false).getAbsolutePath(),
+                getTemplateFromResource("/template01.svg", true).getAbsolutePath(),
                 outputFile.getAbsolutePath()
         });
         System.out.println(FileUtils.readFileToString(outputFile));
     }
 
     @Test
-    public void testRenderWithInstances() throws URISyntaxException, IOException {
+    public void testRenderWithInstances() throws IOException {
         /* TODO: there's an error, if page isn't fully filled, therefore count 200 last item was added,.. fix it!  */
 
         ToolRunner.main( new String[]{
@@ -69,8 +94,8 @@ public class ToolRunnerTest {
                 "--label-size", "65", "26.5",
                 "--label-delta", "0", "0",
                 "--instances-json",
-                Paths.get(getClass().getResource("/test-instances.json").toURI()).toFile().getAbsolutePath(),
-                Paths.get(getClass().getResource("/template01.svg").toURI()).toFile().getAbsolutePath(),
+                getTemplateFromResource("/test-instances.json", false).getAbsolutePath(),
+                getTemplateFromResource("/template01.svg", true).getAbsolutePath(),
                 outputFile.getAbsolutePath()
         });
         System.out.println(FileUtils.readFileToString(outputFile));
